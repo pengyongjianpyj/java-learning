48c7706f9f9729eb3b20660eedf8a20b8d57e769
pengyj <pengyongjianpyj@163.com>
2019-12-30 23:28:50 +0800

    11

--- base-service/src/main/java/com/java/base/utils/StringUtils.java
+++ base-service/src/main/java/com/java/base/utils/StringUtils.java
@@ -6,4 +6,23 @@ package com.java.base.utils;
  * @date 2019-12-30 15:17
  */
 public class StringUtils {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                int i = 100;
+                assert i <  1;
+                System.out.println("assert i < 1;");
+            }
+        });
+        thread.start();
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(100l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("end");
+        }
+
+    }
 }
