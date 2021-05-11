052758af58629f9420f373b41e927c36b0b8e80e
pengyongjian <pengyjd@seentao.com>
2021-05-11 11:05:25 +0800

    more

---
+++ netty-test/src/main/java/com/pyj/netty/start/server/MyChannelHandlerExt.java
@@ -0,0 +1,33 @@
+package com.pyj.netty.start.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author pengyongjian
+ * @Description:
+ * @date 2020/12/17 下午3:03
+ */
+public class MyChannelHandlerExt extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+//        super.channelRead(ctx, msg);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        String msgStr = byteBuf.toString(CharsetUtil.UTF_8);
+        System.out.println("客户端发来数据:" + msgStr);
+        //向客户端发送数据
+        ctx.writeAndFlush(Unpooled.copiedBuffer("ok", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+//        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- netty-test/src/main/java/com/pyj/netty/start/server/MyRPCServer.java
+++ netty-test/src/main/java/com/pyj/netty/start/server/MyRPCServer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * @author pengyongjian
@@ -14,27 +16,29 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  */
 public class MyRPCServer {
     public void start(int port) throws Exception {
-        // 主线程，不处理任何业务逻辑，只是接收客户的连接请求
+        // 主线程池，不处理任何业务逻辑，只是接收客户的连接请求
         NioEventLoopGroup boss = new NioEventLoopGroup(1);
 
-        // 工作线程，线程数默认是:cpu*2
+        // 工作线程池，线程数默认是:cpu*2
         NioEventLoopGroup worker = new NioEventLoopGroup();
         // 服务器启动类
         try {
             ServerBootstrap bootstrap = new ServerBootstrap()
-                    //设置线程组
-                    .group(boss, worker)
-                    //配置server通道
-                    .channel(NioServerSocketChannel.class)
-                    //worker线程的处器
-                    .childHandler(new ChannelInitializer(){
-                        @Override
-                        protected void initChannel(Channel channel) throws Exception {
-                            channel.pipeline()
-                                .addLast(new MyChannelHandler())
-                                .addLast(new MyChannelHandler());
-                        }
-                    });
+                //设置线程组
+                .group(boss, worker)
+                //配置server通道
+                .channel(NioServerSocketChannel.class)
+                //boss线程的处理器
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                //worker线程的处理器
+                .childHandler(new ChannelInitializer(){
+                    @Override
+                    protected void initChannel(Channel channel) {
+                        channel.pipeline()
+                            .addLast(new MyChannelHandler())
+                            .addLast(new MyChannelHandlerExt());
+                    }
+                });
             ChannelFuture channelFuture = bootstrap.bind(port).sync();
             System.out.println("服务器启动完成，端口为:" + port);
             //等待服务端监听端口关闭
