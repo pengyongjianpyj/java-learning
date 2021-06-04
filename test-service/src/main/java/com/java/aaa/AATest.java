cebe22d9a49afd85d2f571ccf7c1195f3bf58291
pengyongjianpyj <pengyongjianpyj@163.com>
2021-06-04 15:46:56 +0800

    more

--- test-service/src/main/java/com/java/aaa/AATest.java
+++ test-service/src/main/java/com/java/aaa/AATest.java
@@ -1,7 +1,6 @@
 package com.java.aaa;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author pengyongjian
@@ -11,43 +10,28 @@ import java.util.List;
 public class AATest {
 
     public static void main(String[] args) {
-        int[] arr = {8, 10, 2};
-        int[] arr2 = {10, 100};
-        int[] arr3 = {10, 110};
-        int[] arr4 = {0, 0};
+        Integer[] arr1 = {8, 10, 2};
+        Integer[] arr2 = {10, 100};
+        Integer[] arr3 = {10, 110};
+        Integer[] arr4 = {0, 10};
 
-        String str = testArrMaxNum(arr);
+        System.out.println(testArrMaxNum(arr1));
+        System.out.println(testArrMaxNum(arr2));
+        System.out.println(testArrMaxNum(arr3));
+        System.out.println(testArrMaxNum(arr4));
     }
 
-    private static String testArrMaxNum(int[] arr) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
-        }
-        list.sort((a, b)->{
-            if (a == 0) {
-                if (b == 0) {
-                    return 0;
-                } else {
-                    return -1;
-                }
-            } else {
-                if (b == 0) {
-                    return 1;
-                } else {
-                    String ab = a + "" + b;
-                    String ba = b + "" + a;
-                    return ab.compareTo(ba);
-                }
-            }
+    private static String testArrMaxNum(Integer[] arr) {
+        Arrays.sort(arr, (a, b)->{
+            String ab = a + "" + b;
+            String ba = b + "" + a;
+            return ba.compareTo(ab);
         });
-        StringBuffer sb = new StringBuffer();
-        sb.append(2);
-
-        StringBuilder sbu = new StringBuilder(12);
-        sbu.append(12);
-
-        return null;
+        StringBuilder str = new StringBuilder();
+        for (Integer integer : arr) {
+            str.append(integer);
+        }
+        return str.toString();
     }
 
 
