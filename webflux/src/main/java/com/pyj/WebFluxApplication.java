b05b06106cb71825ff2b139670fed48fb9ff5974
pengyongjianpyj <pengyongjianpyj@163.com>
2021-10-05 10:46:15 +0800

    1

--- webflux/src/main/java/com/pyj/WebFluxApplication.java
+++ webflux/src/main/java/com/pyj/WebFluxApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebFluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
+		SpringApplication.run(WebFluxApplication.class, args);
 	}
 
 }
